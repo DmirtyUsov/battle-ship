767dc25c0692fc8a11073e6f584fc58e74b5dd50
Dima Usov <dmitry.usov@yahoo.com>
2025-05-15 23:34:32 +0400

    feat: add config

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { httpServer } from './src/http_server/index.js';
+import * as Config from './src/config.js';
+//const HTTP_PORT = 8181;
 
-const HTTP_PORT = 8181;
-
-console.log(`Start static http server on the ${HTTP_PORT} port!`);
-httpServer.listen(HTTP_PORT);
+console.log(`Start static http server on the ${Config.HTTP_PORT} port!`);
+httpServer.listen(Config.HTTP_PORT);
