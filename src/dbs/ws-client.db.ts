ba74670b22744b1150f7924fe413d0107bf37469
Dima Usov <dmitry.usov@yahoo.com>
2025-05-16 21:08:27 +0400

    feat: add wsClientDB

---
+++ src/dbs/ws-client.db.ts
@@ -0,0 +1,16 @@
+import { MapDB } from './map-db';
+import { WebSocketClient } from '../models';
+
+class WsClientDB extends MapDB<number, WebSocketClient> {
+  private nextClientId: number = 0;
+
+  add(item: WebSocketClient): WebSocketClient {
+    item.id = this.nextClientId;
+    const newItem = this.update(this.nextClientId, item);
+    this.nextClientId += 1;
+    console.log('After add',newItem.id);
+    return newItem;
+  }
+}
+
+export const wsClientDB = new WsClientDB();
